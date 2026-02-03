10272057f4c3a239c70c5bdfe0d69c27e06e15fc
Basnat ALBASHITI <Basnat.ALBASHITI@student.hel.be>
2026-02-03 21:31:35 +0100

    Changer le texte du bouton.Modifier la police, la taille et la couleur d'affichage.

--- Dev1/Form1.Designer.cs
+++ Dev1/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -42,11 +43,23 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "l1";
             // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.SteelBlue;
+            this.button1.Location = new System.Drawing.Point(63, 134);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 91);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "b1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "F1";
@@ -58,6 +71,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
